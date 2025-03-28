2151d641163a6577dc57475a05747003a42003b7
CopperyTexas <dengleboff@yandex.ru>
2025-03-28 22:05:17 +0500

    исправил

--- fitness-landing/src/App.jsx
+++ fitness-landing/src/App.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import About from './components/about'
-import Contact from './components/contact'
-import Footer from './components/footer'
-import Header from './components/header'
-import Hero from './components/hero'
-import Services from './components/services'
+import React from 'react';
+import About from './components/about';
+import Contact from './components/contact';
+import Footer from './components/footer';
+import Header from './components/header';
+import Hero from './components/hero';
+import Services from './components/services';
+import Clients from './components/clients.jsx';
 
 function App() {
-	return (
-		<div className=' bg-cover bg-center bg-no-repeat bg-[url("./assets/background.jpg")] relative'>
-			<div className='bg-black bg-opacity-50 h-full w-full absolute top-0 left-0 z-0'></div>
-			<div className='relative z-10 p-4'>
-				<Header />
-				<main>
-					<Hero />
-					<About />
-					<Services />
-					<Contact />
-				</main>
-				<Footer />
-			</div>
-
-		</div>
-	)
+  return (
+    <div className=' bg-cover bg-center bg-no-repeat bg-[url("./assets/background.jpg")] relative'>
+      <div className="bg-black bg-opacity-50 h-full w-full absolute top-0 left-0 z-0"></div>
+      <div className="relative z-10 p-4">
+        <Header />
+        <main>
+          <Hero />
+          <About />
+          <Services />
+          <Clients />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
